b882abbcf39d66d65c0352a0d0c77c6f916f9279
Victor <victorh.csilva20@gmail.com>
2024-09-16 08:03:22 -0300

    criação do leitov_5.py junto da explicão em markdow, o arquivo ecplicacao_v5.md

--- leitos_v4.py
+++ leitos_v4.py
@@ -10,245 +10,119 @@ class Leito():
         self.noite = noite
         self.data = data
         self.DATA = date.today()
-    
-    def _leito(self):
-        return self.identificador
-    
-    def ver_ocupacao(self):
-        return self.identificador,self.manha,self.tarde,self.noite,self.data
-    
-#_____Consultas para um horário e dia________    
-    def _agendar_manha_(self):    
-        data = self.DATA#input('DATA: ')
-        paciente = {
-            'data_inicial':data,
-            'data_final':data,
-            'data_de_nacimento':input('Data de nascimento: '),
-            'cpf':input('CPF: '),
-            'contato':input('Contato: '),#SMS?
-            'endereco':input('Endereço: '),
-            'horario':'manha',
-        }
-        #Se já tiver paciente na lista depacientes do horário da manhã
         
-        if len(self.manha) > 0:
-            
-            agenda_diponivel = False
-            
-            for pacientes_agendados in self.manha:
-                if paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
-                    #Se a data está diponivel entao:
-                    agenda_diponivel = True
-            
-            #Aí podemos adicionar
-            if agenda_diponivel:
-                self.manha.append(paciente)
-            else:
-                print('\nJá existe uma paciente para esta dia, tente agendar para outro')
-                
-        #Se não tiver pacientes na lista de pacientes da manhã
-        else:
-            self.manha.append(paciente)
-
-    def _agendar_tarde_(self):
-        data = self.DATA#input('DATA: ')
-        paciente = {
-            'data_inicial':data,
-            'data_final':data,
-            'nome':input('Nome: '),
-            'data_de_nacimento':input('Data de nascimento: '),
-            'cpf':input('CPF: '),
-            'contato':input('Contato: '),#SMS?
-            'endereco':input('Endereço: '),
-            'horario':'manha',
-        }
-        #Se já tiver paciente na lista depacientes do horário da manhã
-        
-        if len(self.tarde) > 0:
-            
-            agenda_diponivel = False
-            
-            for pacientes_agendados in self.tarde:
-                if paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
-                    #Se a data está diponivel entao:
-                    agenda_diponivel = True
-            
-            #Aí podemos adicionar
-            if agenda_diponivel:
-                self.tarde.append(paciente)
-            else:
-                print('\nJá existe uma paciente para esta dia, tente agendar para outro')
-                
-        #Se não tiver pacientes na lista de pacientes da manhã
-        else:
-            self.tarde.append(paciente)
-
-    def _agendar_noite_(self):
-        data = self.DATA#input('DATA: ')
-        
-        self.paciente = {
-            'data_inicial':data,
-            'data_final':data,
-            'nome':input('Nome: '),
-            'data_de_nacimento':input('Data de nascimento: '),
-            'cpf':input('CPF: '),
-            'contato':input('Contato: '),#SMS?
-            'endereco':input('Endereço: '),
-            'horario':'manha',
-        }
-        
-        #Se já tiver paciente na lista depacientes do horário da manhã
-    
-    def salvar_manha(self ):            
-        if len(self.manha) > 0:
-            
-            agenda_diponivel = False
-            
-            for pacientes_agendados in self.manha:
-                if self.paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
-                    #Se a data está diponivel entao:
-                    agenda_diponivel = True
-            
-            #Aí podemos adicionar
-            if agenda_diponivel:
-                self.manha.append(self.paciente)
-            else:
-                print('\nJá existe uma paciente para esta dia, tente agendar para outro')
-                
-        #Se não tiver pacientes na lista de pacientes da manhã
-        else:
-            self.noite.append(self.paciente)
-
-    #Salvar nas listas 
-    def salvar_tarde(self ):            
-        if len(self.noite) > 0:
-            
-            agenda_diponivel = False
-            
-            for pacientes_agendados in self.tarde:
-                if self.paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
-                    #Se a data está diponivel entao:
-                    agenda_diponivel = True
-            
-            #Aí podemos adicionar
-            if agenda_diponivel:
-                self.tarde.append(self.paciente)
-            else:
-                print('\nJá existe uma paciente para esta dia, tente agendar para outro')
-                
-        #Se não tiver pacientes na lista de pacientes da manhã
-        else:
-            self.noite.append(self.paciente)
-
-    def salvar_noite(self ):            
-        if len(self.noite) > 0:
-            
-            agenda_diponivel = False
-            
-            for pacientes_agendados in self.noite:
-                if self.paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
-                    #Se a data está diponivel entao:
-                    agenda_diponivel = True
-            
-            #Aí podemos adicionar
-            if agenda_diponivel:
-                self.tarde.append(self.paciente)
-            else:
-                print('\nJá existe uma paciente para esta dia, tente agendar para outro')
-                
-        #Se não tiver pacientes na lista de pacientes da manhã
-        else:
-            self.noite.append(self.paciente)
-
-    def ver_leitos_disponiveis(self):
-        if self.manha:
-            for leito_horario_manha in self.manha:
-                if leito_horario_manha:
-                    print('Nome:',leito_horario_manha['nome'],'data:',leito_horario_manha['data'])
-                    
-        else:
-           print('Não existem agendamentos para esse horário') 
-           
-        if self.tarde:
-            for leito_horario_tarde in self.tarde:
-                if leito_horario_tarde:
-                    print('Nome:',leito_horario_tarde['nome'],'data:',leito_horario_tarde['data'])
-        else:
-           print('Não existem agendamentos para esse horário') 
-        
-        if self.noite:
-            for leito_horario_noite in self.noite:
-                if leito_horario_noite:
-                    print('Nome:',leito_horario_noite['nome'],'data:',leito_horario_noite['data'])
-            
-        else:
-           print('Não existem agendamentos para esse horário')    
-    
-#______________Agendar por longo tempo_________
-
-    def _agendar_(self, data_inicial:date,data_final,nome:str,data_de_nascimento:date,cpf:str,contato:str,endereco:str, dias:int):
-        
+    def _agendar_por_horario_(self,horario:int):
         try:
-            for cadastro in range(dias):
-                paciente = {
-                    'data_inicial':data_inicial,
-                    'data_final':data_final,
-                    'nome':nome,
-                    'data_de_nacimento':data_de_nascimento,
-                    'cpf':cpf,
-                    'contato':contato,#SMS?
-                    'endereco':endereco,
-                    'horario':'manha',
-                }
+            if 4 >= horario or horario <= 0:
+                return 'Inválido, O horario deve ser 1, 2 ou 3'
+            
+            if horario == 1:
+                h = 'manha'
+            elif horario == 2:
+                h = 'tarde'
+            elif horario == 3:
+                h = 'noite'
                 
-                paciente = {
-                    'data_inicial':data_inicial,
-                    'data_final':data_final,
-                    'nome':nome,
-                    'data_de_nacimento':data_de_nascimento,
-                    'cpf':cpf,
-                    'contato':contato,#SMS?
-                    'endereco':endereco,
-                    'horario':'tarde',
-                }
-                paciente = {
-                    'data_inicial':data_inicial,
-                    'data_final':data_final,
-                    'nome':nome,
-                    'data_de_nacimento':data_de_nascimento,
-                    'cpf':cpf,
-                    'contato':contato,#SMS?
-                    'endereco':endereco,
-                    'horario':'noite',
-                }
-                
-            print(f'Paciente {nome.title()} está agendado')
+            self.paciente = {
+                'data':input('Data de inicio: '),
+                'nome':input('Nome: '),
+                'data_de_nascimento':input('Data de nascimento: '),
+                'cpf':input('CPF: '),
+                'contato':input('Contato: '),#SMS?
+                'endereco':input('Endereço: '),
+                'horario':h,
+            }
+            
+            self._salvar_consulta(horario=horario) 
+            
         except Exception as erro:
-            print(f'erro: {erro}')
-        
-        #Se já tiver paciente na lista depacientes do horário da manhã
-        
-        if len(self.noite) > 0:
-            
-            agenda_diponivel = False
-            
-            for pacientes_agendados in self.noite:
-                if paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
-                    #Se a data está diponivel entao:
-                    agenda_diponivel = True
-            
-            #Aí podemos adicionar
-            if agenda_diponivel:
-                self.tarde.append(paciente)
-            else:
-                print('\nJá existe uma paciente para esta dia, tente agendar para outro')
+            print(f'Erro: {erro}')
+    
+    def _salvar_consulta(self,horario=int):            
+        #horario da manha
+        if horario == 1:
+            if len(self.manha) > 0:
+                agenda_diponivel = False
                 
-        #Se não tiver pacientes na lista de pacientes da manhã
-        else:
-            self.noite.append(paciente)
-
-lista_de_leitos = []
-
+                for pacientes_agendados in self.manha:
+                    if self.paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
+                        #Se a data está diponivel entao:
+                        agenda_diponivel = True
+                
+                #Aí podemos adicionar
+                if agenda_diponivel:
+                    self.manha.append(self.paciente)
+                    print('\nAgendado!')
+                    return
+                else:
+                    print('\nJá existe uma paciente para esta dia, tente agendar para outro')
+                    
+            #Se não tiver pacientes na lista de pacientes da manhã
+            else:
+                self.manha.append(self.paciente)
+                
+        #horario da tarde        
+        if horario == 2:
+            if len(self.tarde) > 0:
+                
+                for pacientes_agendados in self.tarde:
+                    if self.paciente['data'] == pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
+                        #Se a data está diponivel entao:
+                        return 'Já existe uma paciente para esta dia, tente agendar em outra data'
+                
+                
+                self.tarde.append(self.paciente)    
+            #Se não tiver pacientes na lista de pacientes da manhã
+            else:
+                self.tarde.append(self.paciente)
+                
+        #horario da noite
+        if horario == 3:
+            if len(self.noite) > 0:
+                agenda_diponivel = False
+                
+                for pacientes_agendados in self.noite:
+                    if self.paciente['data'] != pacientes_agendados['data'] :#Varrendo a lista e comparando as datas
+                        #Se a data está diponivel entao:
+                        agenda_diponivel = True
+                
+                #Aí podemos adicionar
+                if agenda_diponivel:
+                    self.noite.append(self.paciente)
+                else:
+                    print('\nJá existe uma paciente para esta dia, tente agendar para outro')
+                    
+            #Se não tiver pacientes na lista de pacientes da manhã
+            else:
+                self.noite.append(self.paciente)
+    
 if __name__=='__main__':
-        #criando 199 leitos:
-    pass
+    lista_de_leitos = []
+    
+    for s in range(1,20):
+        leito = Leito(identificador=f"{s}", data=date.today,manha=[],tarde=[],noite=[])
+        lista_de_leitos.append(leito)
+
+    while True:
+        sair = input('Sair = q: ')
+        escolha = input('numero Leito: ')     
+        
+        for leito in lista_de_leitos:
+            if escolha == leito.identificador:
+                h = input('Insira o horário: ')
+                leito._agendar_por_horario_(int(h))
+            
+        if sair == 'q':
+            break
+        
+    for s in lista_de_leitos:
+        
+        if s.manha:
+            for t in s.manha:
+                print('Manha:',t)
+        elif s.tarde:
+            for t in s.tarde:
+                print('Manha:',t)
+        elif s.noite:
+            for t in s.noite:
+                print('Manha:',t)
